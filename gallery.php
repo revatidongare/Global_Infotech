ecc731af3d3c5b8279bd00800b4c27aca1e60155
revatidongare <revati@vinnovatetechnologies.com>
2020-01-23 21:34:52 +0530

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<?php include 'includes/head.php'; ?>
+
+<title>Global InfoTech : Home</title>
+<!-- <style>
+.mySlides {display: none;}
+</style> -->
+<body class="home-3 gallery-full-width">
+    <div class="all">
+<?php include 'includes/navbar.php'; ?>
+
+    <div class="all gallery-single">
+        
+        <main>
+            <div class="page-title-breadcrumbs">
+                <div class="breadcrumbs-container container">
+                    <div class="breadcrumbs_wrapper">
+                        <div class="page-title-container">
+                            <h1 class="text-center">Gallery</h1>
+                        </div>
+
+                        <div class="breadcrumbs-container text-center">
+                            <ul class="breadcrumbs primary-font">
+                                <li>
+                                    <a href="#">
+                                        <span>Home</span>
+                                    </a>
+                                </li>
+                                <li class="breadcrumb-sep">/</li>
+                                <li>
+                                    <span>Gallery</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section gallery-wrap">
+                <div class="content">
+                    <div class="row m-0">
+                        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 gallery-item clearfix">
+                            <div class="gallery-item-inner clearfix animated">
+                                <div class="entry-thumbnail">
+                                    <div class="entry-thumbnail-overlay placeholder-image" title="gallery"></div>
+                                </div>
+
+                                <div class="gallery-content">
+                                    <div class="gallery-action" data-toggle="modal" data-target="#modal1">
+                                        <a href="#" class="gsf-link">
+                                            <i class="fa fa-expand"></i>
+                                        </a>
+                                    </div>
+
+                                    <div class="gallery-meta">
+                                        <h3 class="gallery-title text-uppercase">
+                                            <a title="gallery">gallery</a>
+                                        </h3>
+                                        <div class="gallery-cat text-italic">
+                                            <a rel="tag">gallery image </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Modal -->
+                            <div class="modal fadeIn zoomIn animated" id="modal1" data-animate="zoomIn" data-delay="50">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="close" data-dismiss="modal">
+                                            <i class="float-right" aria-hidden="true">×</i>
+                                        </div>
+
+                                        <div class="modal-body fadeInDownBig">
+                                            <img src="img/certi/certi_1.jpg" alt="" class="w-100">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 gallery-item clearfix">
+                            <div class="gallery-item-inner clearfix animated">
+                                <div class="entry-thumbnail">
+                                    <div class="entry-thumbnail-overlay placeholder-image img2" title="Gallery"></div>
+                                </div>
+
+                                <div class="gallery-content">
+                                    <div class="gallery-action" data-toggle="modal" data-target="#modal2">
+                                        <a href="#" class="gsf-link">
+                                            <i class="fa fa-expand"></i>
+                                        </a>
+                                    </div>
+
+                                    <div class="gallery-meta">
+                                        <h3 class="gallery-title text-uppercase">
+                                            <a title="Gallery" >gallery </a>
+                                        </h3>
+                                        <div class="gallery-cat text-italic">
+                                            <a rel="tag">gallery image</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Modal -->
+                            <div class="modal fadeIn zoomIn animated" id="modal2" data-animate="zoomIn" data-delay="50">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="close" data-dismiss="modal">
+                                            <i class="float-right" aria-hidden="true">×</i>
+                                        </div>
+
+                                        <div class="modal-body fadeInDownBig">
+                                            <img src="img/certi/certi_2.jpg" alt="" class="w-100">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 gallery-item clearfix">
+                            <div class="gallery-item-inner clearfix animated">
+                                <div class="entry-thumbnail">
+                                    <div class="entry-thumbnail-overlay placeholder-image img3" title="Gallery"></div>
+                                </div>
+
+                                <div class="gallery-content">
+                                    <div class="gallery-action" data-toggle="modal" data-target="#modal3">
+                                        <a href="#" class="gsf-link">
+                                            <i class="fa fa-expand"></i>
+                                        </a>
+                                    </div>
+
+                                    <div class="gallery-meta">
+                                        <h3 class="gallery-title text-uppercase">
+                                            <a title="Gallery">gallery</a>
+                                        </h3>
+                                        <div class="gallery-cat text-italic">
+                                            <a  rel="tag">gallery imge</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Modal -->
+                            <div class="modal fadeIn zoomIn animated" id="modal3" data-animate="zoomIn" data-delay="50">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="close" data-dismiss="modal">
+                                            <i class="float-right" aria-hidden="true">×</i>
+                                        </div>
+
+                                        <div class="modal-body fadeInDownBig">
+                                            <img src="img/certi/certi_3.jpg" alt="" class="w-100">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 gallery-item clearfix">
+                            <div class="gallery-item-inner clearfix animated">
+                                <div class="entry-thumbnail">
+                                    <div class="entry-thumbnail-overlay placeholder-image img4" title="Gallery"></div>
+                                </div>
+
+                                <div class="gallery-content">
+                                    <div class="gallery-action" data-toggle="modal" data-target="#modal6">
+                                        <a href="#" class="gsf-link">
+                                            <i class="fa fa-expand"></i>
+                                        </a>
+                                    </div>
+
+                                    <div class="gallery-meta">
+                                        <h3 class="gallery-title text-uppercase">
+                                            <a title="Gallery">Gallery</a>
+                                        </h3>
+                                        <div class="gallery-cat text-italic">
+                                            <a rel="tag">Gallery Image</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Modal -->
+                            <div class="modal fadeIn zoomIn animated" id="modal6" data-animate="zoomIn" data-delay="50">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="close" data-dismiss="modal">
+                                            <i class="float-right" aria-hidden="true">×</i>
+                                        </div>
+
+                                        <div class="modal-body fadeInDownBig">
+                                            <img src="img/certi/certi_4.jpg" alt="" class="w-100">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <span class="text-center d-block m-top">
+                        <a href="#" class="btn view button-main">Load More</a>
+                    </span>
+                </div>
+            </div>
+        </main>
+
+     
+<?php include 'includes/footer.php'; ?>
+    </div>
+
+    <!-- Back-To-Top Button -->
+    <div class="back-to-top">
+        <a href="#">
+            <i class="fa fa-long-arrow-up"></i>
+        </a>
+    </div>
+
+    <!-- Page Loader -->
+    <div id="page-preloader">
+        <div class="page-loading">
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+        </div>
+    </div>
+
+    <!-- Libs JS -->
+   <?php include 'includes/script.php'; ?>
+</body>
+
+</html>
