1c80205b9cb546974b0113af030a293bc20ecc33
Akshay Kurhekar <akshay.kurhekar1014662@gamil.com>
2020-02-07 20:53:33 +0530

    updated database

--- mcqportal/DB/mcqportal.sql
+++ mcqportal/DB/mcqportal.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 05, 2020 at 08:29 AM
+-- Generation Time: Feb 07, 2020 at 04:15 PM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 7.0.2
 
@@ -39,7 +39,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`user_id`, `name`, `email`, `password`, `phone`) VALUES
-(1, 'Akshay kurhekar', 'akshay.kurhekar1014662@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '09552259961');
+(1, 'admin', 'admin@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '09552259961');
 
 -- --------------------------------------------------------
 
@@ -56,50 +56,6 @@ CREATE TABLE `answers` (
   `remark` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `answers`
---
-
-INSERT INTO `answers` (`id`, `question_id`, `user`, `student_answer`, `actual_answer`, `remark`) VALUES
-(158, 8, 12, 3, 3, 1),
-(159, 8, 13, 0, 3, 0),
-(160, 8, 13, 0, 3, 0),
-(161, 8, 13, 0, 3, 0),
-(162, 8, 13, 2, 3, 0),
-(163, 8, 13, 1, 3, 0),
-(164, 8, 13, 1, 3, 0),
-(165, 8, 13, 1, 3, 0),
-(166, 8, 13, 2, 3, 0),
-(167, 8, 13, 2, 3, 0),
-(168, 2, 13, 1, 2, 0),
-(169, 3, 13, 2, 2, 1),
-(170, 6, 13, 4, 4, 1),
-(171, 7, 13, 1, 1, 1),
-(172, 8, 13, 2, 3, 0),
-(173, 10, 1, 2, 2, 1),
-(174, 10, 1, 2, 2, 1),
-(175, 10, 1, 2, 2, 1),
-(176, 8, 13, 2, 3, 0),
-(177, 0, 1, 0, 0, 1),
-(178, 0, 1, 0, 0, 1),
-(179, 0, 1, 0, 0, 1),
-(180, 10, 1, 2, 2, 1),
-(181, 10, 1, 1, 2, 0),
-(182, 8, 13, 2, 3, 0),
-(183, 10, 8, 2, 2, 1),
-(184, 10, 9, 2, 2, 1),
-(185, 2, 26, 2, 2, 1),
-(186, 3, 26, 2, 2, 1),
-(187, 6, 26, 4, 4, 1),
-(188, 7, 26, 1, 1, 1),
-(189, 2, 13, 2, 2, 1),
-(190, 3, 13, 2, 2, 1),
-(191, 6, 13, 4, 4, 1),
-(192, 7, 13, 1, 1, 1),
-(193, 10, 10, 2, 2, 1),
-(194, 8, 28, 2, 3, 0),
-(195, 11, 2, 2, 2, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -115,29 +71,6 @@ CREATE TABLE `demo_student_data` (
   `subject_id` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `demo_student_data`
---
-
-INSERT INTO `demo_student_data` (`student_id`, `name`, `phone`, `email`, `school`, `subject_id`) VALUES
-(12, 'aksahy', '1234567890', 'akshay@akshay.com', 'Sit', 0),
-(13, 'Akshay kurhekar', '0955225996', 'akshay.kurhekar1014662@gmail.com', 'SIT', 0),
-(14, 'Akshay kurhekar', '0955225996', 'akshay.kurhekar1014662@gmail.com', 'SIT', 0),
-(15, 'Akshay kurhekar', '24', 'akshay.kurhekar1014662@gmail.com', 'Indo public school', 0),
-(16, 'Akshay kurhekar', '2474', 'akshay.kurhekar1014662@gmail.com', 'Indo public school', 0),
-(17, 'Akshay kurhekar', '2474', 'akshay.kurhekar1014662@gmail.com', 'Indo public school', 0),
-(18, 'fvrgvcfsdcccef', '145156', 'akshay.kurhekar1014662@gmail.com', 'Indo public school', 0),
-(19, 'fvrgvcfsdcccef', '145156', 'akshay.kurhekar1014662@gmail.com', 'Indo public school', 0),
-(20, 'Akshay kurhekar', '65146851', 'akshay.kurhekar1014662@gmail.com', 'Indo public school', 0),
-(21, 'Akshay kurhekar', '5687465321', 'akshay.kurhekar1014662@gmail.com', 'Indo public school', 0),
-(22, 'Akshay kurhekar', '09552259961', 'akshay.kurhekar1014662@gmail.com', 'Indo public school', 0),
-(23, 'Akshay kurhekar', '09552259961', 'akshay.kurhekar1014662@gmail.com', 'Indo public school', 0),
-(24, 'Akshay kurhekar', '09552259961', 'akshay.kurhekar1014662@gmail.com', 'Indo public school', 0),
-(25, 'Akshay kurhekar', '09552259961', 'akshay.kurhekar1014662@gmail.com', 'Indo public school', 0),
-(26, 'sham', '1234665', 'sham@gmail.com', 'sppu', 0),
-(27, 'test', '123456923', 'akshay.kurhekar1014662@gmail.com', 'Indo public school', 0),
-(28, 'Akshay kurhekar', '09552259961', 'akshay@gmail.com', 'Indo public school', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -152,16 +85,6 @@ CREATE TABLE `final_student_data` (
   `phone` varchar(20) NOT NULL,
   `subject_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `final_student_data`
---
-
-INSERT INTO `final_student_data` (`test_id`, `school`, `name`, `email`, `phone`, `subject_id`) VALUES
-(2, 'SIT', 'Akshay kurhekar', 'akshay.kurhekar1014662@gmail.com', '+919552259961', 1),
-(9, 'SIT', 'sham', 'sham@gmail.com', '12346', 1),
-(10, 'SIT', 'test', 'test@gmail.com', '1234567890', 1),
-(11, 'Indo public school', 'Akshay kurhekar', 'akshay.kurhekar1014662@gmail.com', '09552259961', 2);
 
 -- --------------------------------------------------------
 
@@ -214,7 +137,7 @@ INSERT INTO `question` (`question_id`, `subject_id`, `question`, `option1`, `opt
 (9, 3, 'question of java ti so simple', 'option 1', 'option 1', 'option 3', 'option 4', 1, 0),
 (10, 1, 'this is final test quetion', 'C', 'C', 'C++', 'Python', 1, 1),
 (11, 2, 'this is final test quetion of c++', 'c', 'c++', 'java', 'python', 2, 1),
-(12, 3, 'java question', 'java', 'bskdjhv', 'vkjhv', 'kjhv', 1, 1);
+(12, 3, 'java question', 'java', '.net', 'PHP', 'node js', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -229,60 +152,6 @@ CREATE TABLE `student_mcq_test` (
   `scored` varchar(255) NOT NULL,
   `total` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `student_mcq_test`
---
-
-INSERT INTO `student_mcq_test` (`id`, `subject_id`, `user_id`, `scored`, `total`) VALUES
-(2, 1, 1, '2', '4'),
-(3, 1, 3, '2', '4'),
-(4, 3, 3, '1', '1'),
-(5, 2, 3, '1', '1'),
-(6, 1, 4, '1', '4'),
-(7, 2, 3, '1', '1'),
-(27, 1, 1, '4', '4'),
-(28, 1, 1, '4', '4'),
-(29, 1, 1, '0', '4'),
-(30, 2, 1, '0', '1'),
-(31, 1, 1, '4', '4'),
-(32, 1, 5, '3', '4'),
-(33, 2, 5, '0', '1'),
-(34, 3, 1, '0', '1'),
-(35, 3, 1, '0', '1'),
-(36, 2, 5, '0', '1'),
-(37, 3, 9, '0', '1'),
-(38, 3, 9, '0', '1'),
-(39, 3, 9, '0', '1'),
-(40, 3, 9, '0', '1'),
-(41, 3, 9, '0', '1'),
-(42, 1, 10, '3', '4'),
-(43, 1, 11, '0', '4'),
-(44, 2, 12, '1', '1'),
-(45, 0, 13, '0', '1'),
-(46, 2, 13, '0', '1'),
-(47, 2, 13, '0', '1'),
-(48, 2, 13, '0', '1'),
-(49, 2, 13, '0', '1'),
-(50, 1, 13, '3', '4'),
-(51, 2, 13, '0', '1'),
-(52, 1, 1, '1', '1'),
-(53, 1, 1, '1', '1'),
-(54, 1, 1, '1', '1'),
-(55, 2, 13, '0', '1'),
-(56, 2, 1, '1', '1'),
-(57, 2, 1, '1', '1'),
-(58, 3, 1, '1', '1'),
-(59, 1, 1, '1', '1'),
-(60, 1, 1, '0', '1'),
-(61, 2, 13, '0', '1'),
-(62, 1, 8, '1', '1'),
-(63, 1, 9, '1', '1'),
-(64, 1, 26, '4', '4'),
-(65, 1, 13, '4', '4'),
-(66, 1, 10, '1', '1'),
-(67, 2, 28, '0', '1'),
-(68, 2, 2, '1', '1');
 
 -- --------------------------------------------------------
 
@@ -370,17 +239,17 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=196;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=201;
 --
 -- AUTO_INCREMENT for table `demo_student_data`
 --
 ALTER TABLE `demo_student_data`
-  MODIFY `student_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `student_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `final_student_data`
 --
 ALTER TABLE `final_student_data`
-  MODIFY `test_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `test_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `key_table`
 --
@@ -395,7 +264,7 @@ ALTER TABLE `question`
 -- AUTO_INCREMENT for table `student_mcq_test`
 --
 ALTER TABLE `student_mcq_test`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 --
 -- AUTO_INCREMENT for table `subject_master`
 --
